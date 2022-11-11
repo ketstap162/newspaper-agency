2169bc9371fa49a6d5acd021770b476d70bfba49
Zakhar Podporinov <zp162162@gmail.com>
2022-11-11 19:47:12 +0200

    Update views.py and index.html

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from newspaper.models import Redactor, Newspaper, Topic
+
 
 def index(request):
-    return render(request, "newspaper/index.html")
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = 1 + request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
