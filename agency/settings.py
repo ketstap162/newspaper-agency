1c86ab8894a401c4536201c4e65facf8d460bc9b
Zakhar Podporinov <zp162162@gmail.com>
2022-11-13 19:46:30 +0200

    Delete default SECRET_KEY

--- agency/settings.py
+++ agency/settings.py
@@ -22,10 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-rnhb^4t!jyn61l77gpu)f%(=(-*0y1pgvzywx^73*&)4s98ex0"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
